4af249bf67e196a5a262a8487dea1b3f8acfa6d5
CHARLOTTE GOLDSBOROUGH <V8197660@live.tees.ac.uk>
2021-10-05 13:31:41 +0100

    added new class

---
+++ EmployeeManager/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager
+{
+    class ClassEmployee { 
+
+        String EmployeName;
+        String EmployeeID;
+        int HoursWorked;
+        double HoulyRate = 9.5;
+       override
+        public String toString(String EmployeeName, String EmployeeID)
+        {
+
+        }
+
+       
+        {
+
+        }
+    }
+}
